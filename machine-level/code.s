2b355cb3d23d8f870425532d5d138e60829ef633
fluorinedog <fluorinedog@gmail.com>
2017-03-18 01:14:59 +0800

    init

---
+++ machine-level/code.s
@@ -0,0 +1,21 @@
+	.file	"code.c"
+	.text
+	.globl	multstore
+	.type	multstore, @function
+multstore:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movq	%rdx, %rbx
+	call	mult2
+	movq	%rax, (%rbx)
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	multstore, .-multstore
+	.ident	"GCC: (GNU) 6.2.1 20160830"
+	.section	.note.GNU-stack,"",@progbits
