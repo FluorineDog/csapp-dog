6d7c180e5aea2272e2183f89a6d9e4023e3d8766
FluorineDog <fluorine@gmail.com>
2018-02-17 12:03:23 +0800

    add_verifier

---
+++ buflab/check.sh
@@ -0,0 +1,5 @@
+cat done0.txt | ./hex2raw | ./bufbomb -u fluorinedog 
+cat done1.txt | ./hex2raw | ./bufbomb -u fluorinedog 
+cat done2.txt | ./hex2raw | ./bufbomb -u fluorinedog 
+cat done3.txt | ./hex2raw | ./bufbomb -u fluorinedog 
+cat done4.txt | ./hex2raw | ./bufbomb -u fluorinedog -n
