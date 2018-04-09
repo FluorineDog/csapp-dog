d0c69f4656b9b840edc99d740308c447ff3fbfd0
FluorineDog <fluorine@gmail.com>
2018-04-09 19:33:10 +0800

    cache-init

--- cachelab/driver.py
+++ cachelab/driver.py
@@ -1,4 +1,4 @@
-#!/usr//bin/python
+#!/usr//bin/python2
 #
 # driver.py - The driver tests the correctness of the student's cache
 #     simulator and the correctness and performance of their transpose
